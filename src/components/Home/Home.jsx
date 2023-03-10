6c140c380151735b41923089c007c9f655cca9a9
Guillaume Sere <guillaumesere@hotmail.fr>
2023-03-10 12:11:00 +0100

    responsive

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './home.css'
 import img from '../../images/100.png'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     
   return (
     <div className='home'>
         <h1 className='title'>Galerie d'<span>Images</span> Abstrait</h1>
-        <img src={img} alt="tableau abstrait" />
+        <Link to='/galerie-1'><img src={img} alt="tableau abstrait" /></Link>
     </div>
   )
 }
