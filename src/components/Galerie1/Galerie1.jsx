2bb965165a11af62d4234d4140b4506b4209a489
Guillaume Sere <guillaumesere@hotmail.fr>
2024-05-20 16:13:57 +0200

    modif animation galerie

--- src/components/Galerie1/Galerie1.jsx
+++ src/components/Galerie1/Galerie1.jsx
@@ -1,39 +1,43 @@
-import React, { useEffect, useState } from 'react'
-import './galerie1.css'
-import { data1 } from '../../data/data'
-import Modal from '../Modal/Modal'
+import React, { useEffect, useState } from 'react';
+import './galerie1.css';
+import { data1 } from '../../data/data';
+import Modal from '../Modal/Modal';
 
 const Galerie1 = () => {
-
     const [randomizedData, setRandomizedData] = useState([]);
 
-       useEffect(() => {
+    useEffect(() => {
         // Générer l'ordre aléatoire une seule fois lors du montage du composant
         const randomOrder = [...data1].sort(() => Math.random() - 0.5);
         setRandomizedData(randomOrder);
     }, []);
 
-    if (document.body.style.overflow = 'hidden'){
+    if (document.body.style.overflow === 'hidden') {
         document.body.style.overflow = 'overlay';
-       }
+    }
 
     return (
         <div className='container-galerie-1'>
-            {randomizedData.map(({ id, image }) => {
-                return (
-                    <>
+            <div className='scrolling-container'>
+                <div className='scrolling-container-inner'>
+                    {randomizedData.map(({ id, image }) => (
                         <div className='card-galerie-1' key={id}>
                             <div className="card-img-galerie-1">
-                            <Modal image={image} openWindow={true} key={id} />
+                                <Modal image={image} openWindow={true} key={id} />
                             </div>
                         </div>
-                      
-                    </>
-                )
-            })
-            }
+                    ))}
+                    {randomizedData.map(({ id, image }) => (
+                        <div className='card-galerie-1' key={`dup-${id}`}>
+                            <div className="card-img-galerie-1">
+                                <Modal image={image} openWindow={true} key={`dup-${id}`} />
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
-export default Galerie1
+export default Galerie1;
